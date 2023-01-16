a64f463d1b708fd4f8ca07ad410b507362609be7
NeilKleistGao <neilkleist@gmail.com>
2023-01-16 21:39:45 +0800

    Add emacs support

--- caviar.el
+++ caviar.el
@@ -7,10 +7,31 @@
 ;; <https://www.gnu.org/licenses/>
 
 
-;;; @file diff_test.cc
+;;; @file caviar.el
 
-(defgroup caviar nil
-  "A simple tool that helps you keep track of your personal expenses in Emacs."
-  :group 'convenience)
+(defvar caviar-keywords
+  '(("Account" . 'font-lock-type-face)
+    ("Plan'" . 'font-lock-type-face)
+    ("Plan" . 'font-lock-type-face)
+    ("Balance" . 'font-lock-type-face)
+    ("\\[.*\\]" . 'font-lock-keyword-face)
+    ("^\\*\\* .*" . 'font-lock-string-face)
+    ("[0-9]+" . 'font-lock-warning-face)
+    ("\\." . 'font-lock-warning-face)
+    ("---" . 'font-lock-string-face)
+    ("[a-zA-Z]+\\:" . 'font-lock-keyword-face))
+  "")
 
+(define-derived-mode caviar-mode fundamental-mode
+  "caviar" "A simple tool that helps you keep track of your personal expenses in Emacs."
+  (setq-default font-lock-defaults '(caviar-keywords t)))
 
+(add-to-list 'auto-mode-alist '("\\.fsd" . caviar-mode))
+
+(global-set-key
+    (kbd "C-c C-a")
+    (lambda ()
+      (interactive)
+      (async-shell-command (concat "caviar -s " (buffer-file-name)))
+    )
+  )
