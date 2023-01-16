c8135a9f6c76306490bcc4477278b949910e049b
NeilKleistGao <neilkleist@gmail.com>
2023-01-16 15:10:01 +0800

    Add options parsing

--- main.cc
+++ main.cc
@@ -20,6 +20,22 @@
 
 /// @file main.cc
 
+#include "third-parties/cxxopts/include/cxxopts.hpp"
+#include "caviar.h"
+
 int main(int argc, char* argv[]) {
+  cxxopts::Options options("caviar",
+                           "A simple tool that helps you keep track of your personal expenses in Emacs.");
+  options.add_options()
+    ("s,summarize", "Summarize your current expenses.", cxxopts::value<std::string>());
+  // TODO: add more params
+
+  auto result = options.parse(argc, argv);
+  if (result.count("summarize") > 0) {
+    std::string filename = result["summarize"].as<std::string>();
+    caviar::truncate(filename);
+    caviar::summarize(std::move(filename));
+  }
+
   return 0;
 }
