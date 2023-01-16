3020db4619ea54a9163fbc6cb1231142c2c07622
NeilKleistGao <neilkleist@gmail.com>
2023-01-16 17:17:24 +0800

    Update License

---
+++ caviar.el
@@ -0,0 +1,16 @@
+;; ---
+;; GNU GENERAL PUBLIC LICENSE
+;; Version 3, 29 June 2007
+;;
+;; Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+;; For more information on this, and how to apply and follow the GNU GPL, see
+;; <https://www.gnu.org/licenses/>
+
+
+;;; @file diff_test.cc
+
+(defgroup caviar nil
+  "A simple tool that helps you keep track of your personal expenses in Emacs."
+  :group 'convenience)
+
+
